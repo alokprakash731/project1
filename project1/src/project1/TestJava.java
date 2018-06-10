4d33f58afe375b6737c13f1debe0d0ab7230aca2
dell <dell@dell-PC>
2018-06-10 23:28:59 +0530

    Added TestJava class

---
+++ project1/src/project1/TestJava.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package project1;
+
+/**
+ * @author dell
+ *
+ */
+public class TestJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("welcome to Our AprraisalMind Team");
+	}
+
+}
